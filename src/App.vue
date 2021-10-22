eb9c80f96df19a8eb7c196f67908371fb2d5e119
民 杨 <luckyoung@certbase.com>
2021-10-22 22:50:21 +0800

    update:Dialog Component

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import Dialog from './components/Dialog/index.vue'
+
+const visible = ref(false)
+</script>
+
+<template>
+  <div class="app-container">
+    <button class="button" @click="visible = true">打开弹窗</button>
+    <Dialog v-model="visible">
+      <img src="./assets/img.svg" alt="">
+      <p>听说这个按钮很神奇！</p>
+      <div><button class="button">一键三连</button></div>
+      <div><button class="button text-button" @click="visible = false">关闭弹窗</button></div>
+    </Dialog>
+  </div>
+</template>
+
+<style lang="less">
+.app-container {
+  display: grid;
+  place-items: center;
+}
+.dialog-body {
+  img {
+    display: block;
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+  }
+  img + p {
+    margin: 30px 0;
+    color: #6266f5;
+  }
+  .text-button {
+    margin-top: 15px;
+  }
+}
+</style>
---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div 
+    class="dialog" 
+    :class="{ 'is-show': visible }"
+    :style="display ? 'display: none' : ''"
+  >
+
+    <!-- 不透明遮罩 -->
+    <div class="dialog-modal" @click.self="closeDialog"></div>
+
+    <!-- 内容区 -->
+    <div class="dialog-main">
+
+      <div class="dialog-head">
+        <button class="button icon-botton" @click="closeDialog">x</button>
+      </div>
+
+      <div class="dialog-body">
+        <slot></slot>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { defineComponent, onBeforeUnmount, ref, watch } from 'vue'
+export default defineComponent({
+  props: {
+    modelValue: Boolean
+  },
+  emits: ['update:modelValue'],
+  setup(props, ctx) {
+    const visible = ref(props.modelValue)
+    const display = ref(true)
+    let timer = 0
+
+    // 关闭弹窗
+    const closeDialog = () => {
+      clearTimeout(timer)
+      visible.value = false
+      timer = setTimeout(() => {
+        display.value = false
+        clearTimeout(timer)
+      }, 300)
+    }
+
+    watch(() => props.modelValue, (currentValue) => {
+      if (currentValue) {
+        clearTimeout(timer)
+        display.value = false
+        timer = setTimeout(() => {
+          visible.value = currentValue
+          clearTimeout(timer)
+        }, 20)
+      } else {
+        closeDialog()
+      }
+    })
+
+    watch(() => visible.value, (currentValue) => {
+      if (!currentValue) {
+        ctx.emit('update:modelValue', currentValue)
+      }
+    })
+
+    onBeforeUnmount(() => {
+      clearTimeout(timer)
+    })
+    return {
+      visible,
+      display,
+      closeDialog
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+@import '../../style/modal.less';
+</style>
